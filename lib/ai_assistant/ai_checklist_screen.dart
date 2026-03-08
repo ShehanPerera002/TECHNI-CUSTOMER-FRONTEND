30cae45a6144a2eb9132935882275d955e88a205
Isurangi Chandupa <isurangigunerathna@gmail.com>
2026-03-08 16:43:12 +0530

    Add AI Checklist screen UI

---
+++ lib/ai_assistant/ai_checklist_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+//This screen shows safety steps before calling a technician
+class AiChecklistScreen extends StatelessWidget {
+  const AiChecklistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Safety Checklist")
+        ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+           
+            const Text(
+              "Please check the following",
+              style: TextStyle(
+                fontSize: 22, 
+                fontWeight: FontWeight.bold),
+            ),
+
+
+            const SizedBox(height: 20),
+
+            //Checklist items
+            const ListTile(
+              leading: Icon(Icons.check_circle_outline),
+              title: Text("Check power connection."),
+            ),
+
+            const ListTile(
+              leading: Icon(Icons.check_circle_outline),
+              title: Text("Restart the appliance."),
+            ),
+
+            const SizedBox(height: 30),
+
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/technician");
+                },
+                child: const Text("View Recommended Technicians"),
+            )
+            ),
