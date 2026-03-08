0c70687259eb276f595fa1b639324a97f0f881f3
Isurangi Chandupa <isurangigunerathna@gmail.com>
2026-03-08 17:30:50 +0530

    Fix the errors in section title widget

--- lib/widgets/section_title.dart
+++ lib/widgets/section_title.dart
@@ -1,7 +1,6 @@
-import 'package: flutter/material.dart';
+import 'package:flutter/material.dart';
 
-//resuable title widget used in screens
-
+// reusable title widget used in screens
 class SectionTitle extends StatelessWidget {
   final String title;
 
@@ -11,27 +10,6 @@ class SectionTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
-
-      child: Text(
-        title,
-        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
-    );
-  }
-}
-
-//resuable title widget used in screens
-
-class SectionTitle extends StatelessWidget {
-  final String title;
-
-  const SectionTitle({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-
       child: Text(
         title,
         style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
