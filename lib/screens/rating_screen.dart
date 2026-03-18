78ac20fd3687e5eb1f0adf40315258fb6962a864
Isurangi Chandupa <isurangigunerathna@gmail.com>
2026-03-19 00:14:43 +0530

    Create RatingScreen widgets and import

---
+++ lib/screens/rating_screen.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+// Package used to create star rating widget
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+//Package used to pick images from gallery or camera
+import 'package:image_picker/image_picker.dart';
+
+//Rating screen widgets 
+ class RatingScreen extends StatefulWigdget{
+    const RatingScreen({Super.key});
+
+    @override 
+    State<RatingScreen> createState() => _RatingScreenState();
+ }
+ 
